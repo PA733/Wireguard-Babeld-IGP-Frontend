6a04a2cdf8e313cfd468bffb70958ca351a432d5
You Puhe <2291750588@qq.com>
2024-12-28 18:42:13 +0800

    refactor: use hero-btn in global scss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
             Material 3 is the latest version of Google's open-source design
             system. Design and build beautiful, usable products with Material 3.
           </p>
-          <button class="get-started">Get started</button>
+          <button class="hero-btn">Get started</button>
         </div>
         <div class="hero-image">
           <img src="abstract-design.png" alt="Abstract Design" />
